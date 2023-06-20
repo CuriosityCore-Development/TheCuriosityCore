b2b75145755f6c292d940d58b68273cc90429395
RazielMartelus96 <117315226+RazielMartelus96@users.noreply.github.com>
2023-06-20 23:44:16 +0100

    Addition of PrimaryExecutableB.java to increase test functionality

---
+++ src/main/java/io/curiositycore/thecuriositycore/testclasses/testCommand/subs/primaryb/PrimaryExecutableB.java
@@ -0,0 +1,43 @@
+package io.curiositycore.thecuriositycore.testclasses.testCommand.subs.primaryb;
+
+import io.curiositycore.thecuriositycore.commands.interfaces.executables.CommandExecutable;
+import java.util.Collections;
+
+import io.curiositycore.thecuriositycore.testclasses.testCommand.subs.primarya.SubExecutableA;
+import io.curiositycore.thecuriositycore.testclasses.testCommand.subs.primarya.SubExecutableB;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class PrimaryExecutableB extends CommandExecutable {
+    public PrimaryExecutableB() {
+        addSubCommand(new SubExecutableC());
+        addSubCommand(new SubExecutableD());
+    }
+    @Override
+    public void perform(CommandSender sender, String[] commandArguments) {
+
+        Player player = (Player) sender;
+        player.sendMessage(this.getSyntax());
+    }
+    @Override
+    protected String initName() {
+        return "primary_executable_b";
+    }
+
+    @Override
+    protected String initDescription() {
+        return "Is primary executeable A";
+    }
+
+    @Override
+    protected String initSyntax() {
+        return "/greet";
+    }
+
+    @Override
+    protected List<String> determineTabCompletes(String[] arguments) {
+        return Collections.emptyList();
+    }
+}
