b0fb007d2c2b46b6da198288c482777620fad9ae
RazielMartelus96 <117315226+RazielMartelus96@users.noreply.github.com>
2023-07-09 16:49:08 +0100

    Crikey grommet! We've forgotten the palette!

---
+++ src/main/java/io/curiositycore/thecuriositycore/inventorymenus/palette/InventoryPalette.java
@@ -0,0 +1,11 @@
+package io.curiositycore.thecuriositycore.inventorymenus.palette;
+
+import io.curiositycore.thecuriositycore.inventorymenus.buttons.BaseInventoryButton;
+
+import java.util.Map;
+
+public interface InventoryPalette {
+    BaseInventoryButton[] initializePalette();
+
+
+}
