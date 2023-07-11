0613b3c24b0e7b5b1c95f709770bbfed91e2ebb4
ChroniclerDelta <TheChroniclerDelta@outlook.com>
2023-07-11 17:10:21 -0500

    DeltaNotes.
    It is likely I missed some minor things, was reading all of this while in a Discord call...

--- src/main/java/io/curiositycore/thecuriositycore/inventorymenus/handlers/InventoryMenuHandler.java
+++ src/main/java/io/curiositycore/thecuriositycore/inventorymenus/handlers/InventoryMenuHandler.java
@@ -2,7 +2,7 @@ package io.curiositycore.thecuriositycore.inventorymenus.handlers;
 
 import io.curiositycore.thecuriositycore.inventorymenus.buttons.BaseInventoryButton;
 
-
+//TODO: DeltaNote, JavaDocs?
 
 public interface InventoryMenuHandler extends InventoryHandler{
     void decorate();
