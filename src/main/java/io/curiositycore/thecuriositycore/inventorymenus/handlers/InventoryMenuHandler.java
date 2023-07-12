3b41fed2ebe2467cfc0ad2e41f1f69a5a460a0e5
RazielMartelus96 <117315226+RazielMartelus96@users.noreply.github.com>
2023-07-12 03:27:43 +0100

    Update InventoryMenuHandler.java
    Added java documentation to InventoryMenuHandler.java. 
    
    Signed-off-by: RazielMartelus96 <117315226+RazielMartelus96@users.noreply.github.com>

--- src/main/java/io/curiositycore/thecuriositycore/inventorymenus/handlers/InventoryMenuHandler.java
+++ src/main/java/io/curiositycore/thecuriositycore/inventorymenus/handlers/InventoryMenuHandler.java
@@ -2,10 +2,19 @@ package io.curiositycore.thecuriositycore.inventorymenus.handlers;
 
 import io.curiositycore.thecuriositycore.inventorymenus.buttons.BaseInventoryButton;
 
-//TODO: DeltaNote, JavaDocs?
+/**
+* Interface that represents the functionality of a inventory menu handler. 
+*/
 
 public interface InventoryMenuHandler extends InventoryHandler{
+    /**
+    * Decorates the inventory menu with the required buttons. 
+    */
     void decorate();
+
+    /**
+    * Add a button to the inventory menu's button map. 
+    */
     void addButton(BaseInventoryButton buttonToAdd,Integer slot);
 
 }
