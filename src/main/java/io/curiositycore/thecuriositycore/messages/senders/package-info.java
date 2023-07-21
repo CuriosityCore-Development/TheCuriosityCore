8968985806e3c42c68bcb50985e48d7e52e22d5a
ChroniclerDelta <TheChroniclerDelta@outlook.com>
2023-07-21 15:58:59 -0500

    Deletion of Deprecated MessageSender and SenderManager classes

--- src/main/java/io/curiositycore/thecuriositycore/messages/senders/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * The Senders Package of the Messages Package. <br>
- * Designed to define classes with message-sending capabilities with ease.
- */
-package io.curiositycore.thecuriositycore.messages.senders;
