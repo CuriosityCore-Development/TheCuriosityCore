a4752c6ef9ce60ccc5423eda5f8c1a08f58d3069
RazielMartelus96 <117315226+RazielMartelus96@users.noreply.github.com>
2023-07-23 00:29:06 +0100

    Testing UnitTest Functionality For Delta.

--- src/main/java/io/curiositycore/thecuriositycore/TheCuriosityCore.java
+++ src/main/java/io/curiositycore/thecuriositycore/TheCuriosityCore.java
@@ -8,12 +8,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 
 public final class TheCuriosityCore extends JavaPlugin {
-    @Getter
-    private FileConfiguration fileConfiguration;
 
     @Override
     public void onEnable() {
-        this.fileConfiguration = this.getConfig();
+
     }
 
     @Override
---
+++ src/test/java/MessagesTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MessagesTest {
+
+    @Test
+    public void testTest(){
+        int test = 1;
+        int test2 = 2;
+        Assert.assertTrue(test+test2 == 3);
+    }
+
+}
