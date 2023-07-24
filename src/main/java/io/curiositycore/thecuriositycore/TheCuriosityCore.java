5256603905bbca2f355624a00bcd6252fa241621
RazielMartelus96 <117315226+RazielMartelus96@users.noreply.github.com>
2023-07-24 22:46:29 +0100

    Deleted test package within configuration pckage.

--- src/main/java/io/curiositycore/thecuriositycore/TheCuriosityCore.java
+++ src/main/java/io/curiositycore/thecuriositycore/TheCuriosityCore.java
@@ -1,9 +1,5 @@
 package io.curiositycore.thecuriositycore;
 
-
-import io.curiositycore.thecuriositycore.configurations.CachedConfigManager;
-import io.curiositycore.thecuriositycore.configurations.test.configs.managers.ManagerTest;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
